88975e87895bcaf59e399ef1e6cd8cf49d5fc461
meha7hak <mehak.kotarya786@gmail.com>
2026-02-12 01:30:59 +0530

    working -- updating

--- server.js
+++ server.js
@@ -11,4 +11,4 @@ const app = express();
 app.use(express.json());
 app.use("/api/auth", authRoutes);
 
-app.listen(5000, () => console.log("Server running on port 5000")); n
+app.listen(5000, () => console.log("Server running on port 5000")); 
